cb5ed7152c2ed5e4b0195e956281037f4d797ee3
Ahmet <ahmete@mimeda.com.tr>
2025-08-27 11:17:30 +0000

    orfad url update

--- scripts/prod_orfad.js
+++ scripts/prod_orfad.js
@@ -7,7 +7,7 @@ import { sleep } from 'k6';
 
 export function prod_orfad() {
 
-    const url = new URL('https://bidding-mimeda.azurewebsites.net/orfad');
+    const url = new URL('https://adserver.mlink.com.tr/orfad');
     const keywords = ['diş', 'diş macunu', 'diş fırçası', 'süt', 'kahve', 'krema', 'dondurma', 'su', 'zeytin yağı', 'çikolata', 'peynir', 'çay', 'kola', 'salça', 'şampuan', 'pirinç', 'ton balığı', 'maden suyu', 'çamaşır suyu', 'yumuşatıcı'];
     const adunits = [ 'migros-sanalmarket-sponsored-product-1', 'migros-sanalmarket-sponsored-product-4','migros-sanalmarket-sponsored-product-5','migros-sanalmarket-sponsored-product-7','migros-sanalmarket-sponsored-product-9','migros-sanalmarket-sponsored-product-11','migros-sanalmarket-sponsored-product-13'];
 
